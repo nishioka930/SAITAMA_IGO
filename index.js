a693b52edd297224f075080ba27717612461e9ce
Nishioka <user@Manaita248.>
2025-06-02 15:33:10 +0900

    追加

---
+++ index.js
@@ -0,0 +1,11 @@
+let iko = document.getElementById("ikko");
+
+iko.addEventListener("click", function Keisan(){
+let me = document.getElementById("tag");
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+
+let num3 = document.getElementById("num3");
+
+num3.value=aaa(num1.value,num2.value,me.value)
+});
