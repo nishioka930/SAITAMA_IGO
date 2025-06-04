175bdc7e4c9ae0a7d9d067df699bf8fe97851af1
nishioka999 <nishioka.k.938@ms.saitama-u.ac.jp>
2025-06-04 11:25:26 +0900

    秘密裏に東方曲布教用サイトの導入

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ let num3 = document.getElementById("num3");
 
 num3.value=aaa(num1.value,num2.value,me.value)
 });
+
+function askValue() {
+      const userInput = prompt("Je veux montrer à mes semblables un homme dans toute la vérité de la nature; et cet homme, ce sera moi.Je n’ai rien tu de mauvais, rien ajouté de bon ; et s’il m’est arrivé d’employer quelque ornement indifférent, ce n’a jamais été que pour remplir un vide occasionné par mon défaut de mémoire ; j’ai pu supposer vrai ce que je savais avoir pu l’être, jamais ce que je savais être faux.");
+
+      if (userInput == 392781243) {
+        window.location.href = "https://nishioka930.github.io/SAITAMA_IGO/about/kiite/kamikyoku.html";
+      }
+    }
